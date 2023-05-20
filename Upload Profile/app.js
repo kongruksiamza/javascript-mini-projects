a1ef58e073eb0e05c0e18c29e9d33aafaa032a45
KongRuksiam Official <kongruksiamstudio@gmail.com>
2023-05-20 12:44:30 +0700

    Delete Upload Profile directory

--- Upload Profile/app.js
+++
@@ -1,6 +0,0 @@
-const image = document.querySelector("img")
-const input = document.querySelector("input")
-
-input.addEventListener("change",()=>{
-    image.src=URL.createObjectURL(input.files[0])
-})
