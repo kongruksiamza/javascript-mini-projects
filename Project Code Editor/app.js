e2928e57f2cc3fa91227d1f23fb675376ba91e73
KongRuksiam Official <kongruksiamstudio@gmail.com>
2023-05-20 12:44:03 +0700

    Delete Project Code Editor directory

--- Project Code Editor/app.js
+++
@@ -1,8 +0,0 @@
-function displayResult(){
-    let codeHTML = document.getElementById("html").value
-    let codeCSS = document.getElementById("css").value
-    let codeJS = document.getElementById("js").value
-    let resultEl = document.getElementById("result")
-    resultEl.contentDocument.body.innerHTML = `${codeHTML}<style>${codeCSS}</style>`
-    resultEl.contentWindow.eval(codeJS)
-}
